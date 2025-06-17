0eff9bdff5a54b0450411f5b9262aa62da960196
lifpro <ysalissou@gmail.com>
2025-06-17 19:23:05 +0000

    first commit

---
+++ studentsvc/src/main/java/com/technolab/studentsvc/StudentsvcApplication.java
@@ -0,0 +1,13 @@
+package com.technolab.studentsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsvcApplication.class, args);
+	}
+
+}
